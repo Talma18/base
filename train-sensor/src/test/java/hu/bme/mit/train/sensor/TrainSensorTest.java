ccf0308f08b9e919d1d97c4219dad7b3a922cb3a
Matyi <Talma18>
2021-03-23 18:18:51 +0100

    Implemented TrainSensor Test using mockito

--- train-sensor/src/test/java/hu/bme/mit/train/sensor/TrainSensorTest.java
+++ train-sensor/src/test/java/hu/bme/mit/train/sensor/TrainSensorTest.java
@@ -4,16 +4,47 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import static org.mockito.Mockito.*;
+import hu.bme.mit.train.interfaces.TrainController;
+import hu.bme.mit.train.interfaces.TrainSensor;
+import hu.bme.mit.train.interfaces.TrainUser;
 
 public class TrainSensorTest {
 
+    TrainController mockController;
+    TrainSensor sensor;
+    TrainUser mockUser;
+
     @Before
     public void before() {
-        // TODO Add initializations
+        mockController = mock(TrainController.class);
+        mockUser = mock(TrainUser.class);
+        sensor = new TrainSensorImpl(mockController, mockUser);
+
+        sensor.overrideSpeedLimit(50);
     }
 
     @Test
-    public void ThisIsAnExampleTestStub() {
-        // TODO Delete this and add test cases based on the issues
+    public void TestNormalBehaviour() {
+        sensor.overrideSpeedLimit(10);
+        verify(mockUser, times(0)).setAlarmState(true);
+    }
+
+    @Test
+    public void TestAbsoluteUpperMargin() {
+        sensor.overrideSpeedLimit(1000);
+        verify(mockUser, times(1)).setAlarmState(true);
+    }
+
+    @Test
+    public void TestAbsoluteLowerMargin() {
+        sensor.overrideSpeedLimit(-10);
+        verify(mockUser, times(1)).setAlarmState(true);
+    }
+
+    @Test
+    public void TestRelativeMargin() {
+        when(mockController.getReferenceSpeed()).thenReturn(20);
+        sensor.overrideSpeedLimit(5);
+        verify(mockUser, times(1)).setAlarmState(true);
     }
 }
